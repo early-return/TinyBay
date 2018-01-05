469df5db06103041599946f6a4800048cd47fd4f
Zhiqing <i@zhiqing.info>
2018-01-05 23:34:59 +0800

    added torrents adapter

--- app/src/main/java/info/zhiqing/tinybay/MainActivity.java
+++ app/src/main/java/info/zhiqing/tinybay/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity
 
     Toolbar toolbar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/info/zhiqing/tinybay/adapter/TorrentListAdapter.java
+++ app/src/main/java/info/zhiqing/tinybay/adapter/TorrentListAdapter.java
@@ -2,12 +2,16 @@ package info.zhiqing.tinybay.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
+import info.zhiqing.tinybay.R;
 import info.zhiqing.tinybay.entities.Torrent;
+import info.zhiqing.tinybay.util.CategoryUtil;
 
 /**
  * Created by zhiqing on 18-1-5.
@@ -29,23 +33,31 @@ public class TorrentListAdapter extends RecyclerView.Adapter<TorrentListAdapter.
 
     @Override
     public TorrentListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new TorrentListViewHolder(inflater.inflate(R.layout.torrents_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(TorrentListViewHolder holder, int position) {
-
+        Torrent torrent = torrents.get(position);
+        holder.title.setText(torrent.getTitle());
+        String info = CategoryUtil.codeToTitle(torrent.getTypeCode()) + "•" + torrent.getSize() + "•" + torrent.getAuthor();
+        holder.info.setText(info);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return torrents != null ? torrents.size() : 0;
     }
 
     class TorrentListViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView info;
 
         public TorrentListViewHolder(View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.text_title);
+            info = itemView.findViewById(R.id.text_info);
         }
     }
 }
